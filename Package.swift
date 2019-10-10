3f225063e7e77e58f13b07f58b89430042029246
Jeremy Boy <github@jboy.eu>
2019-10-10 10:49:55 +0200

    RxSwift 5

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 			targets: ["RxBarcodeScanner"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.5.0")),
+		.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
 		.package(url: "https://github.com/fizon/BarcodeScanner", .branch("master"))
 	],
 	targets: [
