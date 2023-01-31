ad82fa3c38a414cec8058d3285c3ebae4ef68e63
secadt <fs.github@secadt.eu>
2023-01-31 14:12:56 +0100

    fix required sign, package path

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,19 +9,22 @@ let package = Package(
 		.iOS(.v12)
 	],
 	products: [
-		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "RxBarcodeScanner",
 			targets: ["RxBarcodeScanner"]),
 	],
 	dependencies: [
+        // Dependencies declare other packages that this package depends on.
 		.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-		.package(url: "https://github.com/fizon/BarcodeScanner.git", revision: "6702c09f711840d031d29d92da50d935e8810ff9") //Tag 5.0.4
+        .package(url: "https://github.com/fizon/BarcodeScanner.git", from: "5.0.4"), //Tag 5.0.4 //.revisionItem("6702c09f711840d031d29d92da50d935e8810ff9"
 	],
 	targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "RxBarcodeScanner",
-			dependencies: ["BarcodeScanner", "RxCocoa", "RxSwift"],
-			path: "RxBarcodeScanner"),
+			dependencies: ["BarcodeScanner", "RxSwift", .product(name: "RxCocoa", package: "RxSwift")],
+            path: "Sources"),
 	]
 )
